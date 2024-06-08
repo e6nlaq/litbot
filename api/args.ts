8a96461627f7863002320ae5be2de7abd58088e3
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2024-06-08 12:06:47 +0900

    🐛 `coerce`

--- api/args.ts
+++ api/args.ts
@@ -1,5 +1,5 @@
 import { z } from './zod';
-import { empty, hour, minute } from './scheme';
+import { empty, hour_c, minute_c } from './scheme';
 
 const rand_min = -0x1fffffffffffff;
 const rand_max = 0x1fffffffffffff;
@@ -11,7 +11,7 @@ export const args = {
     '!rsp': empty,
     '!room': empty,
     '!yn': empty,
-    '!usetime': z.tuple([hour, minute, hour, minute]),
+    '!usetime': z.tuple([hour_c, minute_c, hour_c, minute_c]),
 };
 
 export type funcs = keyof typeof args;
--- api/scheme.ts
+++ api/scheme.ts
@@ -3,4 +3,7 @@ import { z } from './zod';
 export const empty = z.string().array().length(0);
 
 export const hour = z.number().min(0).max(23);
+export const hour_c = z.coerce.number().min(0).max(23);
+
 export const minute = z.number().min(0).max(59);
+export const minute_c = z.coerce.number().min(0).max(59);
